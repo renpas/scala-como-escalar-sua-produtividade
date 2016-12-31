7ed23fd2a16fd23963657dffa4ab5223c07ad5cf
Renã Passos de Souza <ren.boxe@gmail.com>
2016-12-31 17:53:46 -0200

    fix compilation problems

---
+++ src/main/scala/br/com/renanpass/flickr/api/error/FlickrError.scala
@@ -0,0 +1,8 @@
+package br.com.renanpass.flickr.api.error
+
+/**
+  * @author rsouza on 31/12/16.
+  */
+abstract class FlickrError(val message:String) {
+
+}
--- src/main/scala/br/com/renanpass/flickr/api/parser/ResponseParser.scala
+++ src/main/scala/br/com/renanpass/flickr/api/parser/ResponseParser.scala
@@ -1,7 +1,12 @@
 package br.com.renanpass.flickr.api.parser
 
+import br.com.renanpass.flickr.api.error._
 import br.com.renanpass.flickr.api.model._
 
-trait ResponseParser {
-  def parse(xmlString: String): Seq[Photo]
+sealed trait ResponseParser {
+  def parse(xmlString: String): Either[FlickrError, Seq[Photo]]
+}
+
+class XmlParser extends ResponseParser {
+  override def parse(xmlString: String): Either[FlickrError, Seq[Photo]] = ???
 }
